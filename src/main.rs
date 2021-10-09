9b65eafdb965b79a97e75a7ad40c0ee1d7e1e316
ROki1988 <roki.swindler@gmail.com>
2021-10-09 20:34:33 +0900

    use itertools

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::{
     app_from_crate, crate_authors, crate_description, crate_name, crate_version, value_t_or_exit,
     Arg,
 };
 use csv::{StringRecord, Writer};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
@@ -26,40 +27,22 @@ fn main() -> Result<()> {
 
     let input: PathBuf = value_t_or_exit!(matches, "input", PathBuf);
 
-    let ps: Vec<Rule> = serde_yaml::from_reader(std::fs::File::open(input)?)?;
+    let rs: Vec<Rule> = serde_yaml::from_reader(std::fs::File::open(input)?)?;
     let mut wtr = Writer::from_writer(std::io::stdout());
 
     // write header
-    for p in ps.iter() {
-        wtr.write_field(p.name.as_str())?;
+    for r in rs.iter() {
+        wtr.write_field(r.name.as_str())?;
     }
     wtr.write_record(None::<&[u8]>)?;
 
-    write_patterns(&ps, &mut StringRecord::new(), &mut wtr)
-}
-
-fn write_patterns<W: std::io::Write>(
-    ps: &[Rule],
-    buf: &mut StringRecord,
-    wrt: &mut Writer<W>,
-) -> Result<()> {
-    match ps {
-        [] => Err(anyhow!("Empty rules. Please check your input.")),
-        [p] => {
-            for x in p.pattern.iter() {
-                let mut current = buf.clone();
-                current.push_field(x);
-                wrt.write_record(&current)?
-            }
-            Ok(())
-        }
-        [p, tail @ ..] => {
-            for x in p.pattern.iter() {
-                let mut current = buf.clone();
-                current.push_field(x);
-                write_patterns(tail, &mut current, wrt)?
-            }
-            Ok(())
-        }
+    for r in rs
+        .iter()
+        .map(|x| x.pattern.iter())
+        .multi_cartesian_product()
+    {
+        wtr.write_record(&StringRecord::from(r))?
     }
+
+    Ok(())
 }
